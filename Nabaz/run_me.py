05ead76adf5ff125b68d4ddec6def8cf2245ca60
TLP <itay.shtober.44@gmail.com>
2025-01-08 11:17:33 +0200

    conver ndarray to csv

--- Nabaz/run_me.py
+++ Nabaz/run_me.py
@@ -1,4 +1,6 @@
 def what_is_the_best_tzevet(tzevet1, tzevet2, tzevet3, tzevet4):
     print("The best Tsevet is Tzevet Shtobeg")
 
-what_is_the_best_tzevet("Tzevet Omer", "Tzevet Teva", "Tzevet Shochat", "Tzevet Shtober")
+
+if __name__ == "__main__":
+    what_is_the_best_tzevet("Tzevet Omer", "Tzevet Teva", "Tzevet Shochat", "Tzevet Shtober")
