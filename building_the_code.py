7b84fd38c77eaba2448028d731e5e95ccfbecf31
GenericShtobegMember <neta.eiger.46@gmail.com>
2025-01-08 11:57:17 +0200

    continue desining skeleton for the code, and shtobers functions inserted

--- building_the_code.py
+++ building_the_code.py
@@ -17,36 +17,34 @@ from convert_to_csv import result_to_csv
 
 def data_to_coordinates(data_line: np.ndarray) ->list:
     """gets data line from macam, and returns absolute location coordinates
-    relative to YABA in meters"""
+    latitude, longtitude, and height"""
     pass
 
 
 def generate_list(macam_name: str) -> list[list]:
     """gets macam name, generates a list of all measurements of it as a list
-    looks like: return [x, y, z, ID]"""
+    looks like: return [lat, long, z, ID]"""
     pass
 
 def merge_lists(list_of_names: list) ->list[list]:
     """gets lists from different macams using 'generate_list' and returns one list
-    of [x, y, z, ID] lists"""
+    of [lat, long, z, ID] lists"""
     pass
 
 def split_to_missiles(list_of_all: list[list]) ->dict:
-    """dfghj"""
-    pass
     """gets the list of all lists of coordinates, and returns a dictionary :
     for each ID which is the key, it matches 2D list of data"""
 
 
 def calculate_where_from(list_of_missile: list[list]) ->list:
-    """gets the key"""
+    """gets the data for a missile, returns where was the launch"""
 
 def calculate_where_to(list_of_missile: list[list]) -> list:
-    pass
+    """gets the data for a missile, returns where it will hit"""
 
 
 def determine_ammunition_type(list_of_missile: list[list]) -> str:
-    pass
+    """gets the data for a missile, returns what is it's type"""
 """def certainty(x, y):
     calculation = y
     certainty_value = x
@@ -64,8 +62,6 @@ def data_from_dict_to_launchplaces(missiles_dict: dict) -> list[list]:
         place= calculate_where_from(missiles_dict[ID])
 
 
-def export_to_excel(data: list[list]) ->None:
-    pass
 def export_to_excel(result: list[list[float, float, str, str]], output_name, DEBUD=False):
     result_to_csv(result, output_name, DEBUD)
 
