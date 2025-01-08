93838fa340c68782546575e8445e5202cb6f039b
arielmanela <ariel.manela.46@gmail.com>
2025-01-08 09:07:26 +0200

    code structure

---
+++ building_the_code.py
@@ -0,0 +1,38 @@
+#we have: excel, equations,
+# parameters: time, range, elevation, azimuth,
+# radial_velocity, range_uncertainty, elevation_uncertainty, azimuth_uncertainty
+# , radial_velocity_uncertainty, ID, macam
+
+#plan:
+#read excel
+#calculate ID
+#divide information per til and macam
+def calculations():
+    pass
+
+def calculate_type():
+    pass
+
+def certainty(x, y):
+    calculation = y
+    certainty_value = x
+    if calculation > x:
+        return False
+    else:
+        return True
+
+def calculate_where_from():
+    pass
+
+def calculate_where_to():
+    pass
+
+
+def create_data_where_from():
+    pass
+
+def create_data_where_to():
+    pass
+
+def export_to_excel():
+    pass
