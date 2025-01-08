0041992bf69830de7546974d5a18c88db38013ea
TLP <itay.shtober.44@gmail.com>
2025-01-08 07:14:40 +0200

    very important change for check shtober

--- check_shtober.py
+++ check_shtober.py
@@ -1,1 +1,4 @@
-print("Tzevet Shtober")
+def what_is_the_best_tzevet(tzevet1, tzevet2, tzewvet3, tzevet4):
+    print("Tzevet Shtobeg")
+
+what_is_the_best_tzevet("Tzevet Omer", "Tzevet Teva", "Tzevet Shochat", "Tzevet Shtober")
